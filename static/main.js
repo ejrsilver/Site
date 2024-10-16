d476cb436a6bad798ee40c471efcb23e904ea08b
ejrsilver <ejrsilver@gmail.com>
2024-10-16 15:29:49 -0400

    Minor changes

--- static/main.js
+++ static/main.js
@@ -2,14 +2,17 @@ const pages = {
   default: {
     url: "/",
     title: "Home",
+    content: undefined,
   },
   projects: {
     url: "/projects",
     title: "Personal Projects",
+    content: undefined,
   },
   skills: {
     url: "/skills",
     title: "Skills and Experience",
+    content: undefined,
   },
 };
 
@@ -20,15 +23,25 @@ const pages = {
 function renderForm(val) {
   const ele = document.getElementById("page-swappable-content");
   if (pages[val] !== undefined && ele !== null) {
-    console.log(pages[val]);
     document.title = pages[val].title;
     try {
       window.history.pushState(val, "", pages[val].url);
     } catch (e) {
       console.log(e);
     }
-    ele.innerHTML = document.getElementById(val).outerHTML;
+
+    if (!pages[val].content) {
+      fetch(`${window.location.origin}/static/${val}.html`).then(
+        function (res) {
+          res.text().then(function (txt) {
+            pages[val].content = txt;
+            ele.innerHTML = pages[val].content;
+          });
+        },
+      );
+    } else {
+      ele.innerHTML = pages[val].content;
+    }
   }
 }
-
 renderForm(window.location.pathname);
