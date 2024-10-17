dc237752141374173ca9e23374b7b5858f0ec6c7
ejrsilver <ejrsilver@gmail.com>
2024-10-17 13:32:52 -0400

    Updating styling, content WIP

--- static/main.js
+++ static/main.js
@@ -1,17 +1,17 @@
 const pages = {
-  default: {
-    url: "/",
+  "/": {
+    name: "default",
     title: "Home",
     content: undefined,
   },
-  projects: {
-    url: "/projects",
+  "/projects": {
+    name: "projects",
     title: "Personal Projects",
     content: undefined,
   },
-  skills: {
-    url: "/skills",
-    title: "Skills and Experience",
+  "/resume": {
+    name: "resume",
+    title: "Resume",
     content: undefined,
   },
 };
@@ -21,27 +21,42 @@ const pages = {
  * @param {string} val
  */
 function renderForm(val) {
-  const ele = document.getElementById("page-swappable-content");
-  if (pages[val] !== undefined && ele !== null) {
-    document.title = pages[val].title;
-    try {
-      window.history.pushState(val, "", pages[val].url);
-    } catch (e) {
-      console.log(e);
-    }
+  if (pages[val] === undefined) {
+    console.error(`Page ${val} does not exist.`);
+    return false;
+  }
+  document.title = pages[val].title;
+  try {
+    window.history.pushState(val, "", val);
+  } catch (e) {
+    console.log(e);
+  }
 
-    if (!pages[val].content) {
-      fetch(`${window.location.origin}/static/${val}.html`).then(
-        function (res) {
-          res.text().then(function (txt) {
-            pages[val].content = txt;
-            ele.innerHTML = pages[val].content;
-          });
-        },
-      );
-    } else {
-      ele.innerHTML = pages[val].content;
+  if (!pages[val].content) {
+    console.log(`Fetching content`);
+    fetch(`${window.location.origin}/static/${pages[val].name}.html`).then(
+      function (res) {
+        res.text().then(function (txt) {
+          pages[val].content = txt;
+          console.log(`setting content`);
+
+          const ele = document.getElementById("page-swappable-content");
+          if (ele === null) {
+            console.error("Element does not exist");
+            return false;
+          }
+          ele.innerHTML = pages[val].content;
+        });
+      },
+    );
+  } else {
+    const ele = document.getElementById("page-swappable-content");
+    if (ele === null) {
+      console.error("Element does not exist");
+      return false;
     }
+    ele.innerHTML = pages[val].content;
   }
 }
+console.log(window.location.pathname);
 renderForm(window.location.pathname);
